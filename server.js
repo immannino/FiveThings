e1c7354e983038883b1a48872f0b4b0322b17485
alisonthemonster <alison.wyllie@gmail.com>
2017-05-05 00:16:52 -0700

    add date and five fields

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var router = require('./src/router')
 var app = express();
 
 app.use('/static', express.static('static'));
+app.use('/src', express.static('src'));
 app.use('/', router);
 
 app.listen(8080);
---
+++ src/newEntry.js
@@ -0,0 +1,5 @@
+
+
+document.getElementById('date').valueAsDate = new Date();
+
+console.log("helloooooo");
