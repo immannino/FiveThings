a2394f300ee922ff2d57afa60aa67d62a6864449
alisonthemonster <alison.wyllie@gmail.com>
2017-05-18 21:52:39 -0700

    fix a bunch of things

---
+++ oneTimeParsingScript.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+
+import re
+import json
+
+sample_string = '''
+'''
+
+
+output_json = {}
+
+p1 = re.compile('\d{2}\/\d{2}\/\d{2}', flags=re.DOTALL)
+results = p1.findall(sample_string)
+
+for i in range(0, len(results)-1):
+    things_pattern = "%s(.*?)%s" % (results[i].replace("/", "\/"), results[i+1].replace("/", "\/"))
+    output_json[results[i]] = []
+    notes = re.findall(things_pattern, sample_string, re.DOTALL)[0]
+    for note in notes.split("\n"):
+        output_json[results[i]].append(note[1:])
+    while "" in output_json[results[i]]:
+        output_json[results[i]].remove("");
+
+print json.dumps(output_json, indent=4, sort_keys=True)
+
+text_file = open("Output.txt", "w")
+text_file.write(json.dumps(output_json, indent=4, sort_keys=True))
+text_file.close()
