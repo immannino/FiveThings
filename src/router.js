3fa07192c75e6419f965169084ed8a611aa7eb33
alisonthemonster <alison.wyllie@gmail.com>
2017-09-27 19:06:23 -0700

    First implementation of the template selection page

--- src/router.js
+++ src/router.js
@@ -11,4 +11,8 @@ router.get('/analyze', function(req, res) {
 	res.sendFile(path.join(__dirname + '/analyze.html'));
 });
 
+router.get('/templates', function(req, res) {
+	res.sendFile(path.join(__dirname + '/templates.html'));
+});
+
 module.exports = router;
---
+++ static/templates.js
@@ -0,0 +1,16 @@
+var firebase = require("firebase");
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC7amfR9cLvP0zph_Dghfqt3XUzbZTn9IU",
+    authDomain: "fivethings-20a79.firebaseapp.com",
+    databaseURL: "https://fivethings-20a79.firebaseio.com",
+    projectId: "fivethings-20a79",
+    storageBucket: "fivethings-20a79.appspot.com",
+    messagingSenderId: "882067772741"
+  };
+firebase.initializeApp(config);
+var database = firebase.database();
+var provider = new firebase.auth.GoogleAuthProvider();
+
+
