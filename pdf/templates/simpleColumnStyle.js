58b703d5f8b16684da20d96859a1f8a3be677ce6
alisonthemonster <alison.wyllie@gmail.com>
2017-11-06 19:21:06 -0800

    Create new template and update templates page

--- pdf/templates/simpleColumnStyle.js
+++ pdf/templates/simpleColumnStyle.js
@@ -7,9 +7,9 @@ const doc = new pdf.Document({
   height: 7*72,
   width: 5*72
 })
-doc.pipe(fs.createWriteStream('NewColumn.pdf'))
+doc.pipe(fs.createWriteStream('./pdf/samples/simpleColumn.pdf'))
 
-const img = new pdf.Image(fs.readFileSync('./pdf/assets/cover2017black.jpg'))
+const img = new pdf.Image(fs.readFileSync('./pdf/assets/neutralCover.jpg'))
 doc.image(img, {
   height: 424, align: 'center'
 })
@@ -48,13 +48,14 @@ while (i < keys.length) {
 
 function addHeaders(leftDay, rightDay) {
     var font = new pdf.Font(fs.readFileSync('./pdf/fonts/didonesque.otf'))
-    var tr = table.row();
-    var leftHeader = tr.cell().text({textAlign: 'right', fontSize: 32, lineHeight: .65, font: font})
-    leftHeader.add(getRawMonth(leftDay)+"\n"+getRawDay(leftDay)+"\n"+getShortYear(leftDay))
+    var tr = table.row({paddingBottom: 10});
+    var leftHeader = tr.cell().text({fontSize: 21, lineHeight: .65, font: font})
+    leftHeader.add(getDayOfWeek(leftDay)+"\n\n", {fontSize: 16, lineHeight: .65, font: font})
+            .append(getMonth(leftDay)+" "+getDay(leftDay));
     if (rightDay != null) {
-        var rightHeader = tr.cell().text({textAlign: 'right', fontSize: 32, lineHeight: .65, font: font})
-        rightHeader.add(getRawMonth(rightDay)+"\n"+getRawDay(rightDay)+"\n"+getShortYear(rightDay))
-    
+        var rightHeader = tr.cell().text({fontSize: 21, lineHeight: .65, font: font})
+        rightHeader.add(getDayOfWeek(rightDay)+"\n\n", {fontSize: 16, lineHeight: .65, font: font})
+            .append(getMonth(rightDay)+" "+getDay(rightDay));
     } else {
          tr.cell().text("  ");
     }
@@ -62,31 +63,31 @@ function addHeaders(leftDay, rightDay) {
 
 function addThings(leftThings, rightThings) {
     var tr = table.row();
-    var left = tr.cell({paddingLeft: 25});
+    var left = tr.cell({paddingLeft: 20});
     var numbers = buildNumbers()
     addPadding(leftThings)
-    left.image(numbers[0],  { height: .6*pdf.cm, wrap: false, x: 45})
+    left.image(numbers[0],  { height: .4*pdf.cm, wrap: false, x: 45})
     left.text(leftThings[0]+"\n\n", {fontSize: 7.5, padding: 1.25})
-    left.image(numbers[1],  { height: .6*pdf.cm, wrap: false, x: 45})
+    left.image(numbers[1],  { height: .4*pdf.cm, wrap: false, x: 45})
     left.text(leftThings[1]+"\n\n", {fontSize: 7.5, padding: 1.25})
-    left.image(numbers[2],  { height: .6*pdf.cm, wrap: false, x: 45})
+    left.image(numbers[2],  { height: .4*pdf.cm, wrap: false, x: 45})
     left.text(leftThings[2]+"\n\n", {fontSize: 7.5, padding: 1.25})
-    left.image(numbers[3],  { height: .6*pdf.cm, wrap: false, x: 45})
+    left.image(numbers[3],  { height: .4*pdf.cm, wrap: false, x: 45})
     left.text(leftThings[3]+"\n\n", {fontSize: 7.5, padding: 1.25})
-    left.image(numbers[4],  { height: .6*pdf.cm, wrap: false, x: 45})
+    left.image(numbers[4],  { height: .4*pdf.cm, wrap: false, x: 45})
     left.text(leftThings[4], {fontSize: 7.5, padding: 1.25});
-    var right = tr.cell({paddingLeft: 25});
+    var right = tr.cell({paddingLeft: 20});
     if (rightThings != null) {
         addPadding(rightThings)
-        right.image(numbers[0],  { height: .6*pdf.cm, wrap: false, x: 185})
+        right.image(numbers[0],  { height: .4*pdf.cm, wrap: false, x: 185})
         right.text(rightThings[0]+"\n\n", {fontSize: 7.5, padding: 1.25})
-        right.image(numbers[1],  { height: .6*pdf.cm, wrap: false, x: 185})
+        right.image(numbers[1],  { height: .4*pdf.cm, wrap: false, x: 185})
         right.text(rightThings[1]+"\n\n", {fontSize: 7.5, padding: 1.25})
-        right.image(numbers[2],  { height: .6*pdf.cm, wrap: false, x: 185})
+        right.image(numbers[2],  { height: .4*pdf.cm, wrap: false, x: 185})
         right.text(rightThings[2]+"\n\n", {fontSize: 7.5, padding: 1.25})
-        right.image(numbers[3],  { height: .6*pdf.cm, wrap: false, x: 185})
+        right.image(numbers[3],  { height: .4*pdf.cm, wrap: false, x: 185})
         right.text(rightThings[3]+"\n\n", {fontSize: 7.5, padding: 1.25})
-        right.image(numbers[4],  { height: .6*pdf.cm, wrap: false, x: 185})
+        right.image(numbers[4],  { height: .4*pdf.cm, wrap: false, x: 185})
         right.text(rightThings[4], {fontSize: 7.5, padding: 1.25});
     } else {
         right.text().add("   ");
@@ -123,25 +124,37 @@ function addPadding(things) {
     }
 }
 
-function getRawDay(date) {
+//takes in YY/MM/DD and gets day of week
+function getDayOfWeek(date) {
+    var year = parseInt("20" + date.substring(0, 2));
+    var month = parseInt(date.substring(3, 5))-1;
+    var day = parseInt(date.substring(6, 8));
+    var theDate = new Date(year, month, day);
+    console.log(theDate)
+    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    return days[theDate.getDay()];
+}
+
+//takes in YY/MM/DD and gets date with ordinal
+function getDay(date) {
     var day = date.substring(6, 8);
-    day = parseInt(day);
-    return ("0" + day).slice(-2);
+    var dayNum = parseInt(day);
+    return ordinal_suffix_of(dayNum);
 }
 
-function getShortYear(date) {
-    return date.substring(0, 2);
-}
-
-function getYear(date) {
-    var year = date.substring(0, 2);
-    return "20" + year;
-}
-
-function getRawMonth(date) {
-    var month = date.substring(3, 5);
-    month = parseInt(month);
-    return ("0" + month).slice(-2);
+function ordinal_suffix_of(i) {
+    var j = i % 10,
+        k = i % 100;
+    if (j == 1 && k != 11) {
+        return i + "st";
+    }
+    if (j == 2 && k != 12) {
+        return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+        return i + "rd";
+    }
+    return i + "th";
 }
 
 function getMonth(date) {
